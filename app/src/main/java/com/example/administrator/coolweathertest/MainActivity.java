cb5466ac95e521dd5b933711d2c3d188e4e5132a
wyc <18823439696@163.com>
2017-10-10 22:03:26 +0800

    修改代码

--- app/src/main/java/com/example/administrator/coolweathertest/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweathertest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //哈哈哈哈
+        //第二次提交代码
     }
 }
